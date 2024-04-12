60c2be9dec1b73d95277333628b5a2467b4b24fc
Dastan <dastan.kubanuulu1@gmail.com>
2024-04-12 16:13:54 +0600

    das

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "apps/static"),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
